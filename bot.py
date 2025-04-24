fafe46200f82e93176710e3d8262113728942029
Akmaljon <Akmaljon.19@bk.ru>
2025-04-24 16:24:37 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,16 +1,17 @@
 import os
-from telegram.ext import Updater, CommandHandler
-from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-load_dotenv()
-TOKEN = os.getenv("BOT_TOKEN")
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Assalomu alaykum! Kushon Medical Servis laboratoriyasining rasmiy botiga xush kelibsiz.')
 
-def start(update, context):
-    update.message.reply_text("Salom! Kushon Medical Servis botiga xush kelibsiz.")
+def main():
+    TOKEN = os.getenv("BOT_TOKEN")
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
 
-updater = Updater(token=TOKEN, use_context=True)
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    main()
