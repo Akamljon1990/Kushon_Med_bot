9a8ead757ace8481e4f09abb812fe55b0de0b54b
Akmaljon <Akmaljon.19@bk.ru>
2025-04-28 16:18:30 +0500

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,12 +1,16 @@
 from telegram import ReplyKeyboardMarkup
 
-# Asosiy menyu tugmalarining ro‘yxati
 main_menu_keyboard = [
-    ["Tahlillar", "Biz bilan bog‘lanish"],
-    ["Instagram manzil", "Admin bilan bog‘lanish"],
-    ["Tahlil natijalari", "Taklif va shikoyat"],
-    ["Qon topshirishga tayyorgarlik", "IXLA va IFA tekshiruv farqi"]
+    ["Tahlillar haqida ma'lumot", "Qon topshirish qoidalari"],
+    ["Bioximiya haqida", "Klinika haqida"],
+    ["IXLA va IFA tekshuruv farqi"],
+    ["Biz bilan bog'lanish", "Admin bilan bog'lanish"],
+    ["Tahlil natijalarini olish", "Taklif va shikoyatlar"],
+    ["Kitob (Analizlar haqida to‘liq ma'lumot)"],
+    ["Botga foydalanuvchi qo‘shish", "Sizni nima bezovta qilyapti"]
 ]
 
-# ReplyKeyboardMarkup obyekti yaratish (tugmalar bilan)
-main_menu = ReplyKeyboardMarkup(main_menu_keyboard, resize_keyboard=True)
+main_menu = ReplyKeyboardMarkup(
+    keyboard=main_menu_keyboard,
+    resize_keyboard=True
+)
