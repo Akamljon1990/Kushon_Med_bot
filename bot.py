16922b2a4d664ee01051b3655cb98094c5705d43
Akmaljon <Akmaljon.19@bk.ru>
2025-04-28 14:15:13 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,23 +3,23 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Cal
 import os
 
 # Asosiy menyu
+
 from telegram import ReplyKeyboardMarkup
 
 main_menu_keyboard = [
     ["Tahlillar haqida ma'lumot", "Qon topshirish qoidalari"],
     ["Bioximiya haqida", "Klinika haqida"],
-    ["IXLA va IFA tekshuruv farqi"],
+    ["IXLA va IFA tekshiruv farqi"],
     ["Biz bilan bog'lanish", "Admin bilan bog'lanish"],
     ["Tahlil natijalarini olish", "Taklif va shikoyatlar"],
     ["Kitob (Analizlar haqida to‘liq ma'lumot)"],
-    ["Botga foydalanuvchi qo‘shish", "Sizni nima bezovta qilyapti"]
+    ["Botga foydalanuvchi qo‘shish", "Sizni nima bezovta qilmoqda?"]
 ]
 
 main_menu = ReplyKeyboardMarkup(
     keyboard=main_menu_keyboard,
     resize_keyboard=True
 )
-
 # Gormonlar ro'yxati
 hormone_buttons = [
     "TSH", "FT4", "FT3", "Prolaktin", "Estradiol", "Testosteron", "LH", "FSH", "Progesteron", "AMH",
