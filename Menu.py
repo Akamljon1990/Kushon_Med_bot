ac0ab772d7ecd1f25d64b0268cfca045a3e7c17e
Akmaljon <Akmaljon.19@bk.ru>
2025-04-29 13:45:39 +0500

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,16 +1,17 @@
 from telegram import ReplyKeyboardMarkup
 
-main_menu_keyboard = [
-    ["Tahlillar haqida ma'lumot", "Qon topshirish qoidalari"],
-    ["Bioximiya haqida", "Klinika haqida"],
-    ["IXLA va IFA tekshuruv farqi"],
-    ["Biz bilan bog'lanish", "Admin bilan bog'lanish"],
-    ["Tahlil natijalarini olish", "Taklif va shikoyatlar"],
-    ["Kitob (Analizlar haqida to‘liq ma'lumot)"],
-    ["Botga foydalanuvchi qo‘shish", "Sizni nima bezovta qilyapti"]
-]
+def get_main_menu():
+    main_menu_keyboard = [
+        ["Tahlillar haqida ma'lumot", "Qon topshirish qoidalari"],
+        ["Bioximiya haqida", "Klinika haqida"],
+        ["IXLA va IFA tekshiruv farqi"],
+        ["Biz bilan bog'lanish", "Admin bilan bog'lanish"],
+        ["Tahlil natijalarini olish", "Taklif va shikoyatlar"],
+        ["Kitob (Analizlar haqida to‘liq ma’lumot)"],
+        ["Botga foydalanuvchi qo‘shish", "Sizni nima bezovta qilayapti"]
+    ]
 
-main_menu = ReplyKeyboardMarkup(
-    keyboard=main_menu_keyboard,
-    resize_keyboard=True
-)
+    return ReplyKeyboardMarkup(
+        keyboard=main_menu_keyboard,
+        resize_keyboard=True
+    )
